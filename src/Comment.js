be53603615dcc14e4588ce6dbe588eec41bb670d
acm-sp85 <acontell@gmail.com>
2021-05-14 13:08:13 -0400

    commit tree 82a1ec962764a58bd1bd348446bb6a5b9ae29d46

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,40 @@
-//your code here
+import React, { Component } from 'react';
+
+
+
+class X extends Component {
+
+    render() { 
+        console.log(this.props.commentText)
+        return ( 
+            <>
+
+                <p>{this.props.commentText}</p>
+
+
+            </>
+         );
+        }
+        
+        
+    }
+ 
+export default X;
+
+
+
+
+
+
+// function Comment(props) {
+//     return (
+
+
+//         <p>{props.commentText}</p>
+
+//     )
+
+// }
+
+
+// export default Comment;
